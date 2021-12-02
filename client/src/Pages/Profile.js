3ff158d6ae97f381d4afa20e6d5f5b1f2b32e346
Amy Moseman <amoseman18@gmail.com>
2021-12-02 14:43:19 -0500

    added a useNavigate function to replace useHistory from v5 for a redirect in the profille page

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,10 +1,19 @@
 import React from 'react'
+// useNavigate replaces useHistory
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
+    // this is used now as a redirect
+    let navigate = useNavigate();
     return (
         <div>
             This is the PROFILE PAGE.
-        </div>
+            <button
+                onClick={() => {
+                    navigate('/about');
+                }}> {' '}
+                Change to About page!</button >
+        </div >
     )
 }
 
