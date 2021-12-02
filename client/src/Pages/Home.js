12b2b73b66f7d4511ea44cd35809b45985c9d157
Amy Moseman <amoseman18@gmail.com>
2021-12-02 14:18:59 -0500

    added generic pages as components

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            This is the HOME page!
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/Pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+            This is the PROFILE PAGE.
+        </div>
+    )
+}
+
+export default Profile
